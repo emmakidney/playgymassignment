ebed03a65d78ccae25c69769391d28d8d11aeda5
Glitch (hello-webpage) <none>
2020-08-23 18:58:51 +0000

    🏟🌎 Checkpoint ./views/partials/assessment-table.hbs:18014808/431 ./controllers/dashboard.js:18014808/573

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,11 +2,23 @@
 
 const logger = require("../utils/logger");
 
+const assessment1 = { 
+ date :'01/06/2020',
+ weight: '123.3',
+ chest: '45.0',
+ thigh: '12.5',
+ upperarm: '23.3',
+ waist: '32',
+ hips: '38',
+ trend: 'true',
+}
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Assessment Dashboard",
+      assessment: assessment1
     };
     response.render("dashboard", viewData);
   },
