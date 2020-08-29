090d8c2f4540391026d75f2cfe6ca584d1108aec
Glitch (hello-webpage) <none>
2020-08-29 15:20:19 +0000

    😋🌿 Checkpoint ./controllers/dashboard.js:18014808/18 ./views/partials/assessment-table.hbs:18014808/3

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,16 +4,16 @@ const logger = require("../utils/logger");
 
 const homer = {
   title: 'homer',
-  row: [
- date: '01/06/2020',
+  rows: [
+    { date: '01/06/2020',
  weight: '123.3',
  chest: '45.0',
  thigh: '12.5',
  upperarm: '23.3',
  waist: '32',
  hips: '38',
- trend: 'true',
-    ]
+ trend: 'true', },
+    ],
 };
 
 const dashboard = {
