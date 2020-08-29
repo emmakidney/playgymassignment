de89fa64a9fff95993de556867d94f71b8dcd58a
Glitch (hello-webpage) <none>
2020-08-29 15:31:19 +0000

    💒🏟 Checkpoint ./models/assessment-store.js:18014808/316 ./controllers/dashboard.js:18014808/276

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,20 +1,7 @@
 "use strict";
 
-const logger = require("../utils/logger");
-
-const homer = {
-  title: 'homer',
-  rows: [
-    { date: '01/06/2020',
- weight: '123.3',
- chest: '45.0',
- thigh: '12.5',
- upperarm: '23.3',
- waist: '32',
- hips: '38',
- trend: 'true', },
-    ],
-};
+const logger = require('../utils/logger');
+const homer = require('../models/assessment-store.js');
 
 const dashboard = {
   index(request, response) {
---
+++ models/assessment-store.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+const homer = {
+  title: 'homer',
+  rows: [
+    { date: '01.06.2020',
+ weight: '123.3',
+ chest: '45.0',
+ thigh: '12.5',
+ upperarm: '23.3',
+ waist: '32',
+ hips: '38',
+ trend: 'true', },
+    ],
+};
+
+module.exports = homer;
