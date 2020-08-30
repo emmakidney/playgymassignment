9f067db62bc36ef21fb3586abd09f136b4a55f4a
Glitch (hello-webpage) <none>
2020-08-30 11:43:40 +0000

    ⛑👫 Checkpoint ./views/dashboard.hbs:18014808/57 ./models/assessment-store.json:18014808/657 ./models/assessment-store.js:18014808/577

--- models/assessment-store.js
+++ models/assessment-store.js
@@ -1,35 +1,5 @@
 'use strict';
 
-const logger = require('../utils/logger');
-
-const homer = {
-  title: 'homer',
-  rows: [
-    { date: '01.06.2020',
- weight: '123.3',
- chest: '45.0',
- thigh: '12.5',
- upperarm: '23.3',
- waist: '32',
- hips: '38',
- trend: 'true', },
-    ],
-};
-
-const bart = {
-  title: 'bart',
-  rows: [
-    { date: '01.06.2020',
- weight: '120',
- chest: '43.0',
- thigh: '10.5',
- upperarm: '20.3',
- waist: '30',
- hips: '35',
- trend: 'true', },
-    ],
-};
-
-const allAssessments = [homer, bart];
+const allAssessments = require('./assessment-store.json').allAssessments;
 
 module.exports = allAssessments;
