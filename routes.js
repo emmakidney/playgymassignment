fdc7fcb912d69a4d28b2ba1379b9571591f1323d
Glitch (hello-webpage) <none>
2020-09-02 19:52:50 +0000

    🚊🏓 Checkpoint ./views/signup.hbs:18014808/725 ./controllers/accounts.js:18014808/1402 ./routes.js:18014808/840 ./views/partials/menu.hbs:18014808/62

--- routes.js
+++ routes.js
@@ -6,14 +6,22 @@ const router = express.Router();
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const assessment = require("./controllers/assessment.js");
+const accounts = require("./controllers/accounts.js");
 
-router.get("/", dashboard.index);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
 router.get("/assessment/:id", assessment.index);
 router.get("/assessment/:id/deleterow/:rowid", assessment.deleteRow);
 router.get("/dashboard/deleteassessment/:id", dashboard.deleteAssessment);
-
 router.post("/assessment/:id/addrow", assessment.addRow);
+
+
 
 module.exports = router;
