cec4c6d0ee00c5ee8005d50482fa6d377a930749
Glitch (hello-webpage) <none>
2020-09-04 17:36:28 +0000

    ⭐️🏰 Checkpoint ./utils/analytics.js:18014808/571 ./models/user-store.json:18014808/156

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,12 +2,21 @@
 
 const logger = require('../utils/logger');
 const assessmentStore = require('../models/assessment-store');
+const userStore = require('../models/user-store');
 const accounts = require ('./accounts.js');
 const uuid = require('uuid');
 
-const analytiics = {
+const analytics = {
   
-  generateMemberStats(request, response) {
-    
+  bmi(id) {
+    const user = userStore.getUserById(id);
+    const assessment = assessmentStore.getUserAssessments(id);
+    if (assessment.length === 0) {
+      const bmi = user.startingWeight / ((user.height / 100) * (user.height /100));
+      return Math.round(bmi * 100) / 100;
+    } else {
+      const bmi = assessment[assessment.length - 1].weight/ ((user.height / 100) * (user.height / 100));
+      return Math.round
+    }
   }
 }
