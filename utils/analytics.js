d0cef351d130116a0be126f21d3e1649ae14883c
Glitch (hello-webpage) <none>
2020-09-04 11:34:26 +0000

    🙏🙉 Checkpoint ./utils/analytics.js:18014808/221

---
+++ utils/analytics.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const logger = require('../utils/logger');
+const assessmentStore = require('../models/assessment-store');
+const accounts = require ('./accounts.js');
+const uuid = require('uuid');
+
+const analytiics = {
+  
+}
